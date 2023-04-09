bdeb322cc5cd08b4ba6a551726f0bbaf9add7439
Manuela Baecker <manu.baecker@t-online.de>
2023-04-09 14:04:59 +0200

    search field home page, not working

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,16 +2,18 @@ require 'poke-api-v2'
 
 class PokemonController < ApplicationController
   def show
-    @pokemon = PokeApi.get(pokemon: "#{params[:id]}") #getting the Pokemons by ID
+    #@pokemon = PokeApi.get(pokemon: "#{params[:id]}") #getting the Pokemons by ID
+
+    if params[:search]
+      @pokemon_search = Pokemon.find_by("lower(name) = :search OR id = :search", search: params[:search].downcase)
+    else
+      @pokemon = PokeApi.get(pokemon: "#{params[:id]}") #getting the Pokemons by ID
+    end
   end
 
   def next_pokemon
-    current_pokemon_id = params[:id].to_i
-    @next_pokemon_id = current_pokemon_id + 1
-    redirect_to pokemon_path(@next_pokemon_id)
+    # current_pokemon_id = params[:id].to_i
+    # @next_pokemon_id = current_pokemon_id + 1
+    # redirect_to pokemon_path(@next_pokemon_id)
   end
-
-  def previous_pokemon
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :pokemon
-  
+
   devise_for :users
   root to: "pages#home"
   get "/about", to: "pages#about"
   get "pokemon/:id", to: "pokemon#show"
+  get '/pokemon/search', to: 'pokemon#show', as: 'search_pokemon'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
