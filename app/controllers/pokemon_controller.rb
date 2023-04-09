cf134040753618895cd8207ea0ccd316ae3194b7
Manuela Baecker <manu.baecker@t-online.de>
2023-04-09 13:04:12 +0200

    btn up down is working

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -3,12 +3,15 @@ require 'poke-api-v2'
 class PokemonController < ApplicationController
   def show
     @pokemon = PokeApi.get(pokemon: "#{params[:id]}") #getting the Pokemons by ID
-
-    # @pokemon = Pokemon.new(
-    #
-    #
-  
-    #
-    # )
   end
+
+  def next_pokemon
+    current_pokemon_id = params[:id].to_i
+    @next_pokemon_id = current_pokemon_id + 1
+    redirect_to pokemon_path(@next_pokemon_id)
+  end
+
+  def previous_pokemon
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :pokemon
+  
   devise_for :users
   root to: "pages#home"
   get "/about", to: "pages#about"
   get "pokemon/:id", to: "pokemon#show"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
