c7b02ad050fe78208bb36837d5205a33c12a1f6c
Manuela Baecker <manu.baecker@t-online.de>
2023-04-09 14:37:30 +0200

    still problems with search function

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,18 +2,12 @@ require 'poke-api-v2'
 
 class PokemonController < ApplicationController
   def show
-    #@pokemon = PokeApi.get(pokemon: "#{params[:id]}") #getting the Pokemons by ID
+    @pokemon = PokeApi.get(pokemon: "#{params[:id]}") #getting the Pokemons by ID
 
     if params[:search]
-      @pokemon_search = Pokemon.find_by("lower(name) = :search OR id = :search", search: params[:search].downcase)
+      @pokemon = Pokemon.find_by("lower(name) = :search OR id = :search", search: params[:search].downcase)
     else
-      @pokemon = PokeApi.get(pokemon: "#{params[:id]}") #getting the Pokemons by ID
+      @pokemon = PokeApi.get(pokemon: params[:id])
     end
-  end
-
-  def next_pokemon
-    # current_pokemon_id = params[:id].to_i
-    # @next_pokemon_id = current_pokemon_id + 1
-    # redirect_to pokemon_path(@next_pokemon_id)
   end
 end
